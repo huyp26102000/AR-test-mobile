1528aefba8820a188414cea712f9e127c67cf5eb
huyp3 <huyp3@fpt.com.vn>
2022-09-25 23:54:07 +0700

    init project

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+export default () => {
+  return <View style={{flex: 1}}></View>;
+};
